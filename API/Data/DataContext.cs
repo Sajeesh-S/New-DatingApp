726043c68e516b7857e82bb450d291f7f472a400
ssajeeshambadi@gmail.com <ssajeeshambadi@gmail.com>
2021-03-25 19:02:50 +0530

    End of section  8

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace API.Data
         {
         }
         public DbSet<AppUser> Users { get; set; }
+        
     }
 }
---
+++ API/Extentions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace API.Extentions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateTime dob)
+        {
+            var age = DateTime.Today.Year - dob.Year;
+            if (dob.AddYears(age) > DateTime.Today)
+                age--;
+            return age;  
+        }
+    }
+}
